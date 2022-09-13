2f891ece55495d9575b4cb9199bf77f8c15365ce
Joel Kustemo <kustemo.joel@elev.ga.ntig.se>
2022-09-13 12:25:19 +0200

    dawdw

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+Shrek Shrek = new Shrek();
+string named;
+int day = 0;
+
+Console.WriteLine($"Your tamagotchi is named {Shrek.name}");
+Console.WriteLine("Do you want to change the name? Y/N");
+named = Console.ReadLine();
+named = named.ToLower();
+
+if(named == "y"){
+    Console.WriteLine("Type your name for the tamagotchi!");
+    Shrek.name = Console.ReadLine();
+}
+else if(named == "n"){
+
+}
+
+Console.WriteLine($"Name: {Shrek.name}");
+Console.WriteLine($"Take care of {Shrek.name} well");
+Console.WriteLine("Press any key to continue...");
+Console.ReadLine();
+Console.Clear();
+
+while(Shrek.Alive() == true){
+Console.WriteLine("Day: " + day);
+Shrek.Stats();
+Console.WriteLine($"Choose something to do with {Shrek.name}");
+
+Console.WriteLine($"1) Feed {Shrek.name} \n2) Talk to {Shrek.name} \n3) Teach {Shrek.name} a word\n4) Do nothing");
+string action = Console.ReadLine();
+Console.Clear();
+
+if(action == "1"){
+    Shrek.Feed();
+}
+else if(action == "2"){
+    Shrek.Talk();
+}
+else if(action == "3"){
+    Console.WriteLine($"Type a word to teach {Shrek.name}");
+    string teached = Console.ReadLine();
+    Shrek.Teach(teached);
+}
+else Shrek.Tick();
+
+day++;
+}
---
+++ Tamagotchi/Shrek.cs
@@ -0,0 +1,52 @@
+public class Shrek
+{
+    private int hunger = 4;
+    private int boredom = 4;
+    private int hp = 10;
+    private bool alive = true;
+    Random generator = new Random();
+    private List<string> word = new List<string>() {"Donkey" , "Swamp"};
+    private int rnd;
+    public string name = "Shrek";
+
+    public void Feed(){
+        hunger ++;
+        boredom --;
+        if(hunger >= 10)hunger = 10;
+         Console.WriteLine($"{name} gets food and becomes happy");
+    }
+    public void Talk(){
+        int rndWord = generator.Next(word.Count);
+        Console.WriteLine($"{name} says {word[rndWord]}");
+        Rounds();
+    }
+    public void Teach(string teached){
+        Console.WriteLine($"{name} learned your word");
+        word.Add(teached);
+        Rounds();
+    }
+    public void Stats(){
+        Console.WriteLine("*------------*");
+        Console.WriteLine($"Name: {name} \nHunger: {hunger}\nBoredom: {boredom} \nHealth: {hp}");
+        Console.WriteLine("*------------*");
+    }
+    public bool Alive(){
+        return alive;
+    }
+    public void Tick(){
+        rnd = generator.Next(1,2);
+        hunger -=rnd;
+        boredom --;
+
+        if(hunger >= 6){
+            hp--;
+        }
+        else if(hunger <= 0 || boredom <= 0){
+         alive = false;
+        }
+    }
+    private void Rounds(){
+        boredom++;
+        hunger--;
+    }
+}
